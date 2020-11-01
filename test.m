1e33ff53c5438cc16785f5c11ada10a0342cae18
CyanideForBreakfast <dwivedi.utkarsh01@gmail.com>
2020-11-01 05:42:30 +0530

    finalize overlap

--- test.m
+++ test.m
@@ -1,9 +1,10 @@
 range = 200;
-markers =  [70,270; 170,80; 154,323; 116, 182];
+markers =  [70,270; 170,80; 154,323; 116, 182; 100, 100; 120, 420; 60,300; 61,258];
 tic
-    [c,o] = calcCoverage("FloorWork.tif",markers, range)
+    [c,o] = calcCoverageOverlap("FloorWork.tif",markers, range)
 timeElapsed = toc
 %greedy_Removal("FloorWork.tif", [70,270; 170,80; 154,323; 116, 182; 200, 200], 200, 1)
-% tic
-%     combination_Removal("FloorWork.tif", markers, range, 1)
-% timeElapsed = toc
+tic
+     list = combination_Removal("FloorWork.tif", markers, range, 1)
+     [c1,o1] = calcCoverageOverlap("FloorWork.tif",list, range)
+timeElapsed = toc
